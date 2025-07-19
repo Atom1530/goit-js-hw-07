9bb258f9ec9d97bc65595bbbb035b659923ed0d1
Samir Sharif <samirsharif6969@gmail.com>
2025-07-19 19:19:07 -0400

    fixed file

--- js/task-3.js
+++ js/task-3.js
@@ -2,6 +2,11 @@
 // Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) підставляє його поточне значення в span#name-output як ім’я для привітання. Обов’язково очищай значення в інпуті по краях від пробілів . Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".
 
 
+/* <input type="text" id="name-input" placeholder="Please enter your name" />
+<h1>Hello, <span id="name-output">Anonymous</span>!</h1> */
+
+
+
 const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
@@ -14,15 +19,5 @@ input.addEventListener("input", (e) => {
         output.textContent = name;
   }
 })
-/* <input type="text" id="name-input" placeholder="Please enter your name" />
-<h1>Hello, <span id="name-output">Anonymous</span>!</h1> */
-
-// На що буде звертати увагу ментор при перевірці:
-
-// На елементі input#name-input прослуховується подія input
-// Під час набору тексту в інпуті його поточне значення підставляється в span#name-output як ім’я для привітання
-// Значення в інпуті очищене від пробілів по краях
-// Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous"
-
 
 
--- js/task-5.js
+++ js/task-5.js
@@ -1,13 +1,6 @@
 // Напиши скрипт, який змінює колір фону елемента <body> через інлайн-стиль по кліку на button.change-color і задає це значення кольору текстовим вмістом для span.color.
 
 
-
-// <div class="widget">
-//   <p>Background color: <span class="color">-</span></p>
-//   <button type="button" class="change-color">Change color</button>
-// </div>
-
-
 const body = document.querySelector("body");
 const button = document.querySelector(".change-color");
 
@@ -20,7 +13,7 @@ button.addEventListener('click', () => {
     document.querySelector(".color").textContent = color;
 })
 
-// Для генерування випадкового кольору використовуй функцію getRandomHexColor().
+// Для генерування випадкового кольору 
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -28,14 +21,3 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-
-
-// Зверни увагу, що функція getRandomHexColor() повертає колір у hex-форматі, в той час, як колір фону на <body> буде у форматі rgb. Це нормально й не потребує якихось правок.
-
-
-
-// На що буде звертати увагу ментор при перевірці:
-
-// Фон на <body> задається тільки після кліку на button.change-color
-// При кожному кліку на елемент button.change-color фон <body> зафарбовується новим рандомним кольором
-// На <body> і span.color значення одного й того самого кольору
